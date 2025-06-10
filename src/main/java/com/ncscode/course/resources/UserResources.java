6c28ee447ad7fac573ebeccf7389372f1f9fabb5
Nahtanm <nahtanmcs0019@gmail.com>
2025-06-10 17:04:39 -0300

    Service layer, component registration

--- src/main/java/com/ncscode/course/resources/UserResources.java
+++ src/main/java/com/ncscode/course/resources/UserResources.java
@@ -1,20 +1,33 @@
 package com.ncscode.course.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ncscode.course.entities.User;
+import com.ncscode.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Maria", "maria@gmail.com", "398720983", "1234");
-	return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok().body(userService.findAll());
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(userService.findById(id));
+	}
+
 }
---
+++ src/main/java/com/ncscode/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ncscode.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ncscode.course.entities.User;
+import com.ncscode.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		return userRepository.findById(id).get();
+	}
+
+}
