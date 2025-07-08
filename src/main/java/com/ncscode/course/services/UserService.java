8e5b681e84d1a605f535be63574237f1e20f287f
Nahtanm <nahtanmcs0019@gmail.com>
2025-07-08 16:07:40 -0300

    Exception handling - update

--- src/main/java/com/ncscode/course/services/UserService.java
+++ src/main/java/com/ncscode/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.ncscode.course.repositories.UserRepository;
 import com.ncscode.course.services.exceptions.DatabaseExcepiton;
 import com.ncscode.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -41,9 +43,13 @@ public class UserService {
 	}
 	
 	public User update(Integer id, User user) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, user);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, user);
+			return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(e.getMessage());
+		}
 	}
 
 	public void updateData(User entity, User user) {
